8e4b16c47b6fe89050f83edb0307c96777f2b9b2
陈尚友 <1158676158@qq.com>
2021-04-15 21:35:36 +0800

    提交代码

---
+++ code/code_4_15/code_4_15/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void test()
+{
+	static int i = 0;
+	i++;
+	printf("%d", i);
+}
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		test();
+	}
+	return 0;
+}
